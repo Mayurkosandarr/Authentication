632f479bec9822611975a594a96d7be47e44c70e
Mayurkosandarr <mayurkosandaroffice01@gmail.com>
2024-12-05 17:57:10 +0530

    Update serializers.py

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Item
 
-# Serializer for user registration
+
 class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     password = serializers.CharField(write_only=True)
